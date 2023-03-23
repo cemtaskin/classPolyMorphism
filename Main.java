a5dea1bd2547b8334419320a41807498843e3a0c
cemtaskin <cemtaskin82@gmail.com>
2023-03-23 20:51:50 +0300

    initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Program entry point");
+        Dog dog=new Dog();
+        Pig pig = new Pig();
+
+     
+        dog.animalSound();
+        pig.animalSound();
+    }
+}
